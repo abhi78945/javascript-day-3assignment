a11b3b8e27b121f559d94dcb602946616f46de14
abhi78945 <52705278+abhi78945@users.noreply.github.com>
2020-07-18 22:34:19 +0530

    Add files via upload

---
+++ evenodd.js
@@ -0,0 +1,27 @@
+console.log("Conditional Statements");
+let num = Number(prompt("The"));
+
+
+
+if(num%2==0)
+{
+ // let num = Number(prompt("The number entered is"));
+
+ 
+ var a=num;
+ 
+  
+ console.log("the number entererd is " +a  ,"and no is even");
+
+   }
+   else{
+    
+   // let num = Number(prompt("The number entered is"));
+  
+   var a=num;
+  
+       console.log("the number entererd is " +a ,"and no is odd");
+  }
+
+
+
